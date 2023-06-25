90f5d63f32747bd2ad26fc6449fa20a7d42c04cd
eliadaa <62329770+eliadaa@users.noreply.github.com>
2023-06-25 16:43:31 +0300

    adding sensor codes, triggering the water pump custom with values received from the Wi-Fi board

--- arduino_code.ino
+++ arduino_code.ino
@@ -8,17 +8,28 @@
 #define WATERLEVEL_PIN     A4   // 
 #define WATERLEVEL_POWER_PIN 7  // the water level sensor shouldn't be given power constantly  
 
+#define tempCODE         501
+#define humidCODE        502
+#define luminCODE        503
+#define moistureCODE     504
+#define waterlevelCODE   505
+
+
 DHT dht(dhtPin, dhtType);
-float t, h, l, m, w; // temperature, humidity, light-luminosity, moisture, water_level (for the pump)
 
-float moisture_level, li, soil_moisture_precentage, water_level, water_precentage;
-
-const int no_water = 0;                     // water sensor is dry
-const int full_water = 540;                 // fully immersed in water
+float temp, humid, lumin, moist, water;
+float moisture_level, moisture_percentage, water_level, water_precentage, lumin_level, lumin_percentage;
 
 const int open_air_reading = 520; //700;    //calibration data 1, readings when sensor is exposed to air
 const int water_reading = 260;  //280;      //calibration data 2, readings when sensor is exposed to water
 
+const int no_water         =   0;     // calibration data 3, readings when the water level sensor is dry
+const int full_water       = 540;     // calibration data 4, readings when the water level sensor is fully immersed into water
+
+const int no_light         = 0;
+const int brightest        = 1023;
+
+int min_moisture = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -27,71 +38,87 @@ void setup() {
   pinMode(RELAY_PIN, OUTPUT);
   pinMode(WATERLEVEL_POWER_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, HIGH); // stop
-  digitalWrite(WATERLEVEL_POWER_PIN, LOW);
+  digitalWrite(WATERLEVEL_POWER_PIN, LOW); // stop the power to the water level sensor
   dht.begin();
 }
 
 void loop() {
 
-  t = dht.readTemperature();
-  h = dht.readHumidity();
+  temp = dht.readTemperature();
+  humid = dht.readHumidity();
 
-  l = analogRead(LIGHTSENSOR_PIN);
-  li = (float)l / 1023 * 100.0; // getting the percentage // or use map();  
-  
+  // send temp & humid
+  if(!isnan(temp) && !isnan(humid)){  // is the sensor connected?
+    // Serial.print("temp: ");
+    Serial.println(tempCODE);
+    delay(5000);
+    Serial.println(temp);
+    delay(10000);
+
+    // Serial.print("humid: ");
+    Serial.println(humidCODE);
+    delay(5000);
+    Serial.println(humid);
+    delay(10000);
+  } 
+
+  // luminosity
+  lumin_level = analogRead(LIGHTSENSOR_PIN);
+  lumin_percentage = map(lumin_level, 0, 1023, 0, 100); // 0-1023 analog readings (10 bit resolution) converted into percentages
+
+  if(lumin_percentage >= 0 && lumin_percentage <= 100){
+    Serial.println(luminCODE);
+    delay(5000);
+    Serial.println(lumin_percentage);
+    delay(10000);
+  }
+
+
+  // soil moisture
   moisture_level = analogRead(MOISTURESENSOR_PIN);    // read soil moisture sensor  
-  // map(value, fromLow, fromHigh, toLow, toHigh)
-  soil_moisture_precentage = map(moisture_level, open_air_reading, water_reading, 0, 100);
+  moisture_percentage = map(moisture_level, open_air_reading, water_reading, 0, 100);
 
-  m = soil_moisture_precentage;
-
-  digitalWrite(WATERLEVEL_POWER_PIN, HIGH); // turn the sensor ON
-  delay(20);                                // wait 20 ms
-  water_level = analogRead(WATERLEVEL_PIN);
-  digitalWrite(WATERLEVEL_POWER_PIN, LOW);  // turn the sensor OFF
-  water_precentage = map(water_level, no_water, full_water, 0, 100);  // convert in percentage
-  w = water_precentage;
- // Serial.println(moisture_level);
-
-  if(isnan(t) || isnan(h) || isnan(l) || isnan(moisture_level)) 
-    Serial.println("no data available from sensor");
-
-  Serial.print(t);  // send the data from the temperature sensor through serial communication, since esp8266 is connected to Arduino, it will receive the data
-  Serial.print(",");
-  Serial.print(h);  
-  Serial.print(",");
-  Serial.print(li);  
-  Serial.print(",");
-  Serial.print(m);  
-  Serial.print(",");
-  Serial.print(w);
-  Serial.println();
-
-
-  // deal with dry soil
-
-  // !!!!! as far as I tested, the relay works on the opposite logic
-  // starts when setting the pin to LOW 
-  // and stops when setting the pin to HIGH
-  // I am losing it..............
-
-  if(moisture_level > 500 || moisture_level < water_reading){ // use open_air_reading and water_reading
-    // air or water, skip
-    digitalWrite(RELAY_PIN, HIGH);
+  if(moisture_percentage >= 0 && moisture_percentage <= 100){
+    // Serial.print("moisture:");
+    Serial.println(moistureCODE);
+    delay(5000);
+    Serial.println(moisture_percentage);
+    delay(10000);
   }
-  else if(soil_moisture_precentage < 40){  // if the soil is too dry, start the pump by giving power to the relay
-    digitalWrite(RELAY_PIN, LOW);
-    delay(3500); // so the pump will be on, pause the program for 3500 milliseconds
-    digitalWrite(RELAY_PIN, HIGH); // stop
+
+
+  // water level 
+  digitalWrite(WATERLEVEL_POWER_PIN, HIGH);  // turn the sensor ON
+  delay(20);                                 // wait 20 ms
+  water_level = analogRead(WATERLEVEL_PIN);  // read the sensor
+  digitalWrite(WATERLEVEL_POWER_PIN, LOW);   // turn the sensor OFF
+  water_precentage =  map(water_level, no_water, full_water, 0, 100); // convert in percentage
+
+  if(water_precentage >= 0 && water_precentage <= 100){
+    // Serial.print("water_level:");
+    Serial.println(waterlevelCODE);
+    delay(5000);
+    Serial.println(water_precentage);
+    delay(10000);
   }
-  else
-    digitalWrite(RELAY_PIN, HIGH); // in any other case, stop
 
 
 
-  delay(10000);  // stop 2 seconds, maybe increase this 
+  // get min moisture value from the wifi board 
+  if (Serial.available()>0) {
+    String data = Serial.readStringUntil('\n');
+    min_moisture = data.toInt();
 
-
-
+    // compare the value with the actual soil moisture precentage
+    // if the moisture is smaller that the allowed range, trigger the pump 
+    if(moisture_percentage < min_moisture){
+      digitalWrite(RELAY_PIN, LOW);
+      delay(6000); // so the pump will be on, pause the program for 6000 milliseconds
+      digitalWrite(RELAY_PIN, HIGH); // stop
+      delay(15000);
+    }
+  }
+  
+  delay(30000);
 
 }
